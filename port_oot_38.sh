6dea286e4ec90cfb1e0043a57f81dbc6a9735d5e
TheWylieStCoyote <thewyliestcoyote@gmail.com>
2019-09-20 09:30:16 -0700

    Add changes after problems found

--- port_oot_38.sh
+++ port_oot_38.sh
@@ -18,15 +18,15 @@ gr_modtool newmod $MODNAME
 
 cd ../
 
-cp -r $PACKDIR/include/$MODNAME/ port38/$PACKDIR/
-cp -r $PACKDIR/grc/ port38/$PACKDIR/
-cp -r $PACKDIR/lib/ port38/$PACKDIR/
-cp -r $PACKDIR/lib/ port38/$PACKDIR/
-cp -r $PACKDIR/swig/ port38/$PACKDIR/
-cp -r $PACKDIR/docs/ port38/$PACKDIR/
-cp -r $PACKDIR/apps/ port38/$PACKDIR/
-cp -r $PACKDIR/examples/ port38/$PACKDIR/
-cp -r $PACKDIR/.git port38/$PACKDIR/
+cp -nrp $PACKDIR/include/$MODNAME/ port38/$PACKDIR/
+cp -nrp $PACKDIR/grc/ port38/$PACKDIR/
+cp -nrp $PACKDIR/lib/ port38/$PACKDIR/
+cp -nrp $PACKDIR/lib/ port38/$PACKDIR/
+cp -nrp $PACKDIR/swig/ port38/$PACKDIR/
+cp -nrp $PACKDIR/docs/ port38/$PACKDIR/
+cp -nrp $PACKDIR/apps/ port38/$PACKDIR/
+cp -nrp $PACKDIR/examples/ port38/$PACKDIR/
+cp -nrp $PACKDIR/.git port38/$PACKDIR/
 cp $PACKDIR/README.md port38/$PACKDIR/README.md
 cp $PACKDIR/MANIFEST.md port38/$PACKDIR/MANIFEST.md
 cp $PACKDIR/LICENSE port38/$PACKDIR/LICENSE

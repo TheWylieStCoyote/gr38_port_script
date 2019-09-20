fe5654c3e9f8d7d3fafb82f001c704c8ceae05db
TheWylieStCoyote <thewyliestcoyote@gmail.com>
2019-09-20 09:22:03 -0700

    Get working kinda

--- port_oot_38.sh
+++ port_oot_38.sh
@@ -1,9 +1,10 @@
+#!/bin/bash
 
-packdir = $1
+PACKDIR=$1
+PACKDIR=${PACKDIR::-1}
+MODNAME=${PACKDIR:3}
 
-modname = packdir:3
-
-cd $packdir
+cd $PACKDIR
 
 gr_modtool update --complete
 
@@ -13,23 +14,24 @@ mkdir port38
 
 cd port38
 
-gr_modtool newmod $modname
+gr_modtool newmod $MODNAME
 
 cd ../
 
-cp $packdir/include/$modname/* port38/$packdir/include/$modname/
-cp $packdir/grc?* port38/$packdir/grc/
-cp $packdir/lib/*.c port38/$packdir/lib/
-cp $packdir/lib/*.h port38/$packdir/lib/
-cp $packdir/swig/*.i port38/$packdir/swig/
-cp $packdir/docs/* port38/$packdir/docs/
-cp $packdir/apps/* port38/$packdir/apps/
-cp $packdir/example/* port38/$packdir/example/
-cp $packdir/README.md port38/$packdir/README.md
-cp $packdir/MANIFEST.md port38/$packdir/MANIFEST.md
-cp $packdir/LICENSE port38/$packdir/LICENSE
+cp -r $PACKDIR/include/$MODNAME/ port38/$PACKDIR/
+cp -r $PACKDIR/grc/ port38/$PACKDIR/
+cp -r $PACKDIR/lib/ port38/$PACKDIR/
+cp -r $PACKDIR/lib/ port38/$PACKDIR/
+cp -r $PACKDIR/swig/ port38/$PACKDIR/
+cp -r $PACKDIR/docs/ port38/$PACKDIR/
+cp -r $PACKDIR/apps/ port38/$PACKDIR/
+cp -r $PACKDIR/examples/ port38/$PACKDIR/
+cp -r $PACKDIR/.git port38/$PACKDIR/
+cp $PACKDIR/README.md port38/$PACKDIR/README.md
+cp $PACKDIR/MANIFEST.md port38/$PACKDIR/MANIFEST.md
+cp $PACKDIR/LICENSE port38/$PACKDIR/LICENSE
 
-echo "$packdir moved to 3.8 structure"
-echo "Located in ./port38/$packdir"
+echo "$PACKDIR moved to 3.8 structure"
+echo "Located in ./port38/$PACKDIR"
 echo "Now check Python modules and lib/CmakeLists.txt"
 echo "Then build and test till complete"
